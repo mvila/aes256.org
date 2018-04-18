fcbf3af30765a32847bbc5306e2d9b002d32ede6
Manuel Vila <mvila@3base.com>
2018-04-18 14:36:07 +0900

    Initial commit

---
+++ website/src/components/root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Root extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Offline AES 256 encryption</h1>
+      </div>
+    );
+  }
+}
+
+export default Root;
